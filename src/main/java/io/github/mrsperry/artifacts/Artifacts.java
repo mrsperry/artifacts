0bc2fa609a9ddbc7a64c3f4c704a265c08800373
Josh <mrjoshuasperry@gmail.com>
2020-05-09 14:48:43 -0400

    Initial commit

---
+++ src/main/java/io/github/mrsperry/artifacts/Artifacts.java
@@ -0,0 +1,15 @@
+package io.github.mrsperry.artifacts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Artifacts extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
