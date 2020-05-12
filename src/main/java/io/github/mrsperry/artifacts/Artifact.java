0b82cbdd3df9dd6b9e9e40fee9a3cc9ed6d2974f
Josh <mrjoshuasperry@gmail.com>
2020-05-12 00:07:43 -0400

    Add runnable handling to artifacts

--- src/main/java/io/github/mrsperry/artifacts/Artifact.java
+++ src/main/java/io/github/mrsperry/artifacts/Artifact.java
@@ -1,8 +1,16 @@
 package io.github.mrsperry.artifacts;
 
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Artifact {
     /** The current enable state of this artifact */
     private static boolean enabled;
+    /** A map of any runnables and their timers this artifact handles */
+    private static final Map<BukkitRunnable, Integer> runnables = new HashMap<>();
 
     /**
      * Sets the initial enable state of this artifact
@@ -13,11 +21,24 @@ public class Artifact {
     }
 
     /**
-     * Sets the artifact's enable state
+     * Sets the artifact's enable state and starts or stops any handled runnables
      * @param enabled The new enable state
      */
     public static void setEnabled(final boolean enabled) {
+        if (Artifact.enabled == enabled) {
+            return;
+        }
+
         Artifact.enabled = enabled;
+
+        for (final BukkitRunnable runnable : Artifact.runnables.keySet()) {
+            if (enabled) {
+                final int interval = Artifact.runnables.get(runnable);
+                runnable.runTaskTimer(Artifacts.getInstance(), interval, interval);
+            } else {
+                Bukkit.getScheduler().cancelTask(runnable.getTaskId());
+            }
+        }
     }
 
     /**
@@ -25,5 +46,20 @@ public class Artifact {
      */
     public static boolean isEnabled() {
         return Artifact.enabled;
+    }
+
+    /**
+     * Adds a new runnable to be handled by this artifact
+     * <br><br>
+     * Runnables will be started or stopped whenever the artifact is enabled or disabled
+     * @param runnable The runnable to be added
+     * @param interval The interval at which the runnable should be run
+     */
+    public static void addRunnable(final BukkitRunnable runnable, final int interval) {
+        Artifact.runnables.put(runnable, interval);
+
+        if (Artifact.isEnabled()) {
+            runnable.runTaskTimer(Artifacts.getInstance(), interval, interval);
+        }
     }
 }
