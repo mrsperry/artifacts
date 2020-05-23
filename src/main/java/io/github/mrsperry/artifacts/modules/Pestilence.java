538442ed161e9483bf39f7c9f18fd169c151af40
Josh <mrjoshuasperry@gmail.com>
2020-05-23 05:03:37 -0400

    Add gamemode check to Pestilence

--- src/main/java/io/github/mrsperry/artifacts/modules/Pestilence.java
+++ src/main/java/io/github/mrsperry/artifacts/modules/Pestilence.java
@@ -5,7 +5,9 @@ import io.github.mrsperry.mcutils.types.CropTypes;
 
 import net.md_5.bungee.api.ChatColor;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -21,11 +23,17 @@ public class Pestilence extends Artifact implements Listener {
             return;
         }
 
+        final Player player = event.getPlayer();
+        final GameMode gameMode = player.getGameMode();
+        if (gameMode == GameMode.CREATIVE || gameMode == GameMode.SPECTATOR) {
+            return;
+        }
+
         final Material type = event.getBlockPlaced().getType();
 
         // Check if the block places is a plant type
         if (CropTypes.getAllTypes().contains(type)) {
-            event.getPlayer().sendMessage(ChatColor.RED + "The land is infertile and nothing can be planted");
+            player.sendMessage(ChatColor.RED + "The land is infertile and nothing can be planted");
             event.setCancelled(true);
         }
     }
