3bb6a54d669087aaac736054c1f8fa8596eeb5bb
Josh <mrjoshuasperry@gmail.com>
2020-05-28 05:37:55 -0400

    Code cleanup

--- src/main/java/io/github/mrsperry/artifacts/modules/CraftingTimer.java
+++ src/main/java/io/github/mrsperry/artifacts/modules/CraftingTimer.java
@@ -5,6 +5,7 @@ import io.github.mrsperry.artifacts.Artifacts;
 import io.github.mrsperry.artifacts.Config;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,108 +20,127 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.*;
 
 public class CraftingTimer extends Artifact implements Listener {
-    //Max time to craft before dying
-    private final int timeThreshold;
-    private final String[] DEATH_MSGS = {
-            "%player% failed to craft",
-            "Crafting proved to be too much for %player%",
-            "%player% has crafted themselves into the grave",
-            "%player% needed just a bit more time"
+    /** Custom death messages to use when a player dies while crafting */
+    private final String[] deathMessages = {
+        "%player% failed to craft",
+        "Crafting proved to be too much for %player%",
+        "%player% has crafted themselves into the grave",
+        "%player% needed just a bit more time"
     };
-    //Players currently crafting
-    private Map<UUID, BukkitRunnable> crafters;
-    private List<UUID> deadCrafters;
+
+    /** Number of seconds before a player dies while crafting */
+    private final int maxCraftingTime;
+    /** A map of all players currently crafting */
+    private final Map<UUID, BukkitRunnable> crafters;
+    private final List<UUID> deadCrafters;
 
     public CraftingTimer() {
         super("crafting-timer");
-        this.timeThreshold = Config.getInt(this.id, "max-crafting-time", 10) * 20;
+        this.maxCraftingTime = Config.getInt(this.id, "max-crafting-time", 10) * 20;
         this.crafters = new HashMap<>();
         this.deadCrafters = new ArrayList<>();
     }
 
     @EventHandler
-    public void onCraftingTableOpen(InventoryOpenEvent event) {
-        if(this.isEnabled()) {
-            if(event.getInventory().getType().equals(InventoryType.WORKBENCH)) {
-                Player player = (Player) event.getPlayer();
+    private void onCraftingTableOpen(final InventoryOpenEvent event) {
+        if (!this.isEnabled()) {
+            return;
+        }
 
-                //put player as crafting and start runnable
-                crafters.put(player.getUniqueId(), new BukkitRunnable() {
-                    private int elapsed = 0;
-                    private int nextTick = 0;
-                    @Override
-                    public void run() {
-                        if(elapsed >= timeThreshold) {
-                            outOfTime(player);
-                        } else {
-                            //Handles the ticking sound, gets faster as time ticks down
-                            nextTick = tick(player, elapsed, nextTick);
-                        }
-                        elapsed += 1;
+        if (event.getInventory().getType() == InventoryType.WORKBENCH) {
+            final Player player = (Player) event.getPlayer();
+            final UUID id = player.getUniqueId();
+
+            //put player as crafting and start runnable
+            this.crafters.put(id, new BukkitRunnable() {
+                private int elapsedTime = 0;
+                private int nextTick = 0;
+
+                @Override
+                public void run() {
+                    if(this.elapsedTime >= maxCraftingTime) {
+                        outOfTime(player);
+                    } else {
+                        //Handles the ticking sound, gets faster as time ticks down
+                        this.nextTick = tick(player, this.elapsedTime, this.nextTick);
                     }
-                });
-                crafters.get(player.getUniqueId()).runTaskTimer(Artifacts.getInstance(), 0, 1);
-            }
-        }
-    }
 
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        if(this.isEnabled()) {
-            if (event.getEntity().getLastDamageCause().getCause().equals(EntityDamageEvent.DamageCause.CUSTOM)) {
-                //Pick random death message and set it for a player
-                String deathMsg = this.DEATH_MSGS[Artifacts.random(0, this.DEATH_MSGS.length-1)];
-                deathMsg = deathMsg.replace("%player%", event.getEntity().getDisplayName());
-                event.setDeathMessage(deathMsg);
-            }
-        }
-    }
-
-    @EventHandler
-    public void onCraftingTableClose(InventoryCloseEvent event) {
-        if(this.isEnabled()) {
-            UUID uuid = event.getPlayer().getUniqueId();
-            //If player was crafting, cancel runnable and remove from map
-            if(crafters.containsKey(uuid)) {
-                crafters.get(uuid).cancel();
-                crafters.remove(uuid);
-
-                //Special death scenario, killed on crafting inventory close
-                if(deadCrafters.contains(uuid)) {
-                    Player player = (Player) event.getPlayer();
-                    player.getWorld().createExplosion(player.getLocation(), 0);
-                    player.damage(player.getHealth());
-                    deadCrafters.remove(player.getUniqueId());
+                    this.elapsedTime += 1;
                 }
+            });
+
+            this.crafters.get(id).runTaskTimer(Artifacts.getInstance(), 0, 1);
+        }
+    }
+
+    @EventHandler
+    private void onPlayerDeath(final PlayerDeathEvent event) {
+        if (!this.isEnabled()) {
+            return;
+        }
+
+        final Player player = event.getEntity();
+        final EntityDamageEvent lastDamage = player.getLastDamageCause();
+        if (lastDamage != null && lastDamage.getCause() == EntityDamageEvent.DamageCause.CUSTOM) {
+            // Pick random death message
+            final String message = this.deathMessages[Artifacts.random(0, this.deathMessages.length - 1)];
+            // Insert the player's name into the message
+            event.setDeathMessage(message.replace("%player%", player.getDisplayName()));
+        }
+    }
+
+    @EventHandler
+    private void onCraftingTableClose(final InventoryCloseEvent event) {
+        if (!this.isEnabled()) {
+            return;
+        }
+
+        final UUID uuid = event.getPlayer().getUniqueId();
+        // If player was crafting, cancel runnable and remove from map
+        if (this.crafters.containsKey(uuid)) {
+            this.crafters.get(uuid).cancel();
+            this.crafters.remove(uuid);
+
+            // Special death scenario, killed on crafting inventory close
+            if (this.deadCrafters.contains(uuid)) {
+                final Player player = (Player) event.getPlayer();
+                player.getWorld().createExplosion(player.getLocation(), 0);
+                player.damage(player.getHealth());
+                this.deadCrafters.remove(player.getUniqueId());
             }
         }
     }
 
-    private void outOfTime(Player player) {
-        int rand = Artifacts.random(1, 10);
-        //10% chance to get special death scenario
-        if(rand > 1) {
-            //kill player
-            player.getWorld().createExplosion(player.getLocation(), 0);
+    private void outOfTime(final Player player) {
+        final Location location = player.getLocation();
+
+        // 10% chance to get special death scenario
+        if (Artifacts.random(1, 10) > 1) {
+            // Kill the player with an explosion that does no damage
+            player.getWorld().createExplosion(location, 0);
             player.damage(player.getHealth());
         } else {
-            //add player to list so they die when they close crafting inventory
+            // Add player to list so they die when they close crafting inventory
             player.sendMessage(ChatColor.DARK_PURPLE + "Ill leave this one up to you");
-            this.deadCrafters.add(player.getUniqueId());
-            player.playSound(player.getLocation(), Sound.BLOCK_BEACON_DEACTIVATE, 1f, 1f);
-            crafters.get(player.getUniqueId()).cancel();
+            player.playSound(location, Sound.BLOCK_BEACON_DEACTIVATE, 1f, 1f);
+
+            final UUID id = player.getUniqueId();
+            this.deadCrafters.add(id);
+            this.crafters.get(id).cancel();
         }
     }
 
-    //Ticking sound, max (a) ticks apart, min (b) ticks apart
-    private int a = 12, b = 2;
-    private int tick(Player player, int elapsed, int nextTick) {
-        if(elapsed >= nextTick) {
-            double perc = (double) elapsed / this.timeThreshold;
+    // Ticking sound, max (a) ticks apart, min (b) ticks apart
+    private final int max = 12, min = 2;
+    private int tick(final Player player, final int elapsed, final int nextTick) {
+        if (elapsed >= nextTick) {
+            final double percent = (double) elapsed / this.maxCraftingTime;
             player.playSound(player.getLocation(), Sound.BLOCK_METAL_PRESSURE_PLATE_CLICK_ON, 0.5f, 2f);
-            //lerp between max spacing and min spacing given time left
-            return elapsed + (int)Math.floor(a + (perc * (b - a)));
+
+            // Lerp between max spacing and min spacing given time left
+            return elapsed + (int) Math.floor(this.max + (percent * (this.min - this.max)));
         }
+
         return nextTick;
     }
 }
