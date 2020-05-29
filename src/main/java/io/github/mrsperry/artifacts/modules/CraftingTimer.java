332a4272802bf22b8194a466e6a4b624a366ca1b
Josh <mrjoshuasperry@gmail.com>
2020-05-29 03:28:10 -0400

    More code cleanup; add persistent data to check for custom death message

--- src/main/java/io/github/mrsperry/artifacts/modules/CraftingTimer.java
+++ src/main/java/io/github/mrsperry/artifacts/modules/CraftingTimer.java
@@ -4,6 +4,9 @@ import io.github.mrsperry.artifacts.Artifact;
 import io.github.mrsperry.artifacts.Artifacts;
 import io.github.mrsperry.artifacts.Config;
 
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +16,8 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
@@ -38,28 +43,50 @@ public class CraftingTimer extends Artifact implements Listener {
             return;
         }
 
-        if (event.getInventory().getType() == InventoryType.WORKBENCH) {
-            final Player player = (Player) event.getPlayer();
-            final UUID id = player.getUniqueId();
+        final Player player = (Player) event.getPlayer();
+        final GameMode gameMode = player.getGameMode();
+        if (gameMode == GameMode.CREATIVE || gameMode == GameMode.SPECTATOR) {
+            return;
+        }
 
-            //put player as crafting and start runnable
+        if (event.getInventory().getType() == InventoryType.WORKBENCH) {
+            final UUID id = player.getUniqueId();
+            final int maxCraftingTime = this.maxCraftingTime;
+
             this.crafters.put(id, new BukkitRunnable() {
+                /** The amount of ticks that have passed since this runnable has started */
                 private int elapsedTime = 0;
+                /** The target number of ticks used to check when a new ticking sound should be played */
                 private int nextTick = 0;
 
                 @Override
                 public void run() {
-                    if(this.elapsedTime >= maxCraftingTime) {
-                        outOfTime(player);
+                    final Location location = player.getLocation();
+
+                    if (this.elapsedTime >= maxCraftingTime) {
+                        // Mark this player as crafting; used to check if a custom death message should be displayed
+                        final PersistentDataContainer container = player.getPersistentDataContainer();
+                        container.set(new NamespacedKey(Artifacts.getInstance(), "crafting-timer"), PersistentDataType.BYTE, (byte) 0);
+
+                        // Kill the player with an explosion that does no damage
+                        player.getWorld().createExplosion(location, 0);
+                        player.damage(player.getHealth());
                     } else {
-                        //Handles the ticking sound, gets faster as time ticks down
-                        this.nextTick = tick(player, this.elapsedTime, this.nextTick);
+                        if (this.elapsedTime >= this.nextTick) {
+                            player.playSound(location, Sound.BLOCK_METAL_PRESSURE_PLATE_CLICK_ON, 0.5f, 2f);
+
+                            // Get the percentage to use for lerping
+                            final double percent = (double) this.elapsedTime / maxCraftingTime;
+                            // Lerp between max spacing (12) and min spacing (-10) given time left
+                            this.nextTick = this.elapsedTime + (int) Math.floor(12 + (percent * -10));
+                        }
                     }
 
                     this.elapsedTime += 1;
                 }
             });
 
+            // Run the crafting runnable every tick
             this.crafters.get(id).runTaskTimer(Artifacts.getInstance(), 0, 1);
         }
     }
@@ -76,12 +103,23 @@ public class CraftingTimer extends Artifact implements Listener {
         }
 
         final Player player = event.getEntity();
+
+        // Check if this player is marked for a custom death message
+        final PersistentDataContainer container = player.getPersistentDataContainer();
+        final NamespacedKey key = new NamespacedKey(Artifacts.getInstance(), "crafting-timer");
+        if (!container.has(key, PersistentDataType.BYTE)) {
+            return;
+        }
+
         final EntityDamageEvent lastDamage = player.getLastDamageCause();
         if (lastDamage != null && lastDamage.getCause() == EntityDamageEvent.DamageCause.CUSTOM) {
             // Pick random death message
             final String message = this.deathMessages.get(Artifacts.random(0, this.deathMessages.size() - 1));
             // Insert the player's name into the message
             event.setDeathMessage(message.replace("%player%", player.getDisplayName()));
+
+            // Remove the mark for a custom death message
+            container.remove(key);
         }
     }
 
@@ -91,31 +129,11 @@ public class CraftingTimer extends Artifact implements Listener {
             return;
         }
 
-        final UUID uuid = event.getPlayer().getUniqueId();
+        final UUID id = event.getPlayer().getUniqueId();
         // If player was crafting, cancel runnable and remove from map
-        if (this.crafters.containsKey(uuid)) {
-            this.crafters.get(uuid).cancel();
-            this.crafters.remove(uuid);
+        if (this.crafters.containsKey(id)) {
+            this.crafters.get(id).cancel();
+            this.crafters.remove(id);
         }
-    }
-
-    private void outOfTime(final Player player) {
-        // Kill the player with an explosion that does no damage
-        player.getWorld().createExplosion(player.getLocation(), 0);
-        player.damage(player.getHealth());
-    }
-
-    // Ticking sound, max (a) ticks apart, min (b) ticks apart
-    private final int max = 12, min = 2;
-    private int tick(final Player player, final int elapsed, final int nextTick) {
-        if (elapsed >= nextTick) {
-            final double percent = (double) elapsed / this.maxCraftingTime;
-            player.playSound(player.getLocation(), Sound.BLOCK_METAL_PRESSURE_PLATE_CLICK_ON, 0.5f, 2f);
-
-            // Lerp between max spacing and min spacing given time left
-            return elapsed + (int) Math.floor(this.max + (percent * (this.min - this.max)));
-        }
-
-        return nextTick;
     }
 }
