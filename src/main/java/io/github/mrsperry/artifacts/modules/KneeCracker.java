a7e68f1a122d5831a1cfb30186d85d6719034a55
Josh <mrjoshuasperry@gmail.com>
2020-05-31 11:52:53 -0400

    Add config option for KneeCracker to only apply damage modifiers to players

--- src/main/java/io/github/mrsperry/artifacts/modules/KneeCracker.java
+++ src/main/java/io/github/mrsperry/artifacts/modules/KneeCracker.java
@@ -3,22 +3,30 @@ package io.github.mrsperry.artifacts.modules;
 import io.github.mrsperry.artifacts.Artifact;
 import io.github.mrsperry.artifacts.Config;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class KneeCracker extends Artifact implements Listener {
+    /** If the damage multiplier should be applied only to players */
+    private final boolean onlyPlayers;
     /** Multiplier used when applying any fall damage to an entity */
     private final float damageMultiplier;
 
     public KneeCracker() {
         super("knee-cracker");
+        this.onlyPlayers = Config.getBoolean(this.id, "only-players", true);
         this.damageMultiplier = Config.getFloat(this.id, "damage-multiplier", 1.5f);
     }
 
     @EventHandler
     private void onEntityDamage(final EntityDamageEvent event) {
         if (!this.isEnabled()) {
+            return;
+        }
+
+        if (this.onlyPlayers && event.getEntityType() != EntityType.PLAYER) {
             return;
         }
 
